029a4b46c8df94f0036de7a289fc227ebf5596d6
Tom van Onna <145599715+tvanonna@users.noreply.github.com>
2025-03-31 17:22:20 +0200

    Delete ssbs2d directory
    Since the file structure was changed, these files are unecessary duplicates.

--- ssbs2d/entities/entities/ssbs_hitbox/shared.lua
+++
@@ -1,9 +0,0 @@
-
-ENT.Base = "base_gmodentity"
-ENT.PrintName = "Hitbox"
-ENT.Type = "anim" --otherwise not spownable using Lua
-ENT.Spawnable = false
-
-function ENT:OnRemove()
-
-end
--- ssbs2d/gamemode/cl_settings.lua
+++
@@ -1,5 +0,0 @@
-AddCSLuaFile()
-
---the settings menu should be implemented in this file in the future, when there are
---multiple characters to select and actual settings to change
-
